ad67b4204999fcd734f7c553edf0cb08dad69bf6
Alexander Bersenev <bay@hackerdom.ru>
2016-10-25 15:31:17 +0500

    change trafdump destination to the new host

--- ansible/roles/vpn/files/trafdump/del_trafdump_rules.sh
+++ ansible/roles/vpn/files/trafdump/del_trafdump_rules.sh
@@ -3,10 +3,10 @@
 # this script shouldn't be run normally :)
 
 for chain in INPUT FORWARD OUTPUT; do
-    iptables -t mangle -D $chain -s 10.80.0.0/14 -j TEE --gateway 10.10.10.6
-    iptables -t mangle -D $chain -s 10.60.0.0/14 -j TEE --gateway 10.10.10.6
-    iptables -t mangle -D $chain -s 10.10.10.2/32 -j TEE --gateway 10.10.10.6
-    iptables -t mangle -D $chain -s 10.10.10.3/32 -j TEE --gateway 10.10.10.6
-    iptables -t mangle -D $chain -s 10.10.10.4/32 -j TEE --gateway 10.10.10.6
-    iptables -t mangle -D $chain -s 10.10.10.5/32 -j TEE --gateway 10.10.10.6
+    iptables -t mangle -D $chain -s 10.80.0.0/14 -j TEE --gateway 10.10.10.10
+    iptables -t mangle -D $chain -s 10.60.0.0/14 -j TEE --gateway 10.10.10.10
+    iptables -t mangle -D $chain -s 10.10.10.2/32 -j TEE --gateway 10.10.10.10
+    iptables -t mangle -D $chain -s 10.10.10.3/32 -j TEE --gateway 10.10.10.10
+    iptables -t mangle -D $chain -s 10.10.10.4/32 -j TEE --gateway 10.10.10.10
+    iptables -t mangle -D $chain -s 10.10.10.5/32 -j TEE --gateway 10.10.10.10
 done
