e10b4ae0f2d085f3756c3f5993947e4841746eda
Zaynullin Bulat <zaynullin_bulat@skbkontur.ru>
2016-11-05 21:42:53 +0500

    pastebin: generate secret on startup

--- services/pastebin/startup.sh
+++ services/pastebin/startup.sh
@@ -1,8 +1,15 @@
+#!/bin/sh
+
+config=/etc/nginx/config.lua
+
+if test ! -e $config; then
+        secret=`head -c 1024 /dev/urandom | sha512sum`;
+cat > $config <<- END
 local config = require("lapis.config").config
 
 config("development", {
 	session_name = "auth",
-	secret = "qqq",
+	secret = "$secret",
 	redis = {
 		host = "127.0.0.1",
 		port = "6379"
@@ -10,3 +17,5 @@ config("development", {
 	ttl = 1200,
 	show_time = 10,
 })
+END
+fi
