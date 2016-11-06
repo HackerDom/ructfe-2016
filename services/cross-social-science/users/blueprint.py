84b91818f4f1b1a11be20784acc15f0b9689f413
pahaz <pahaz.blinov@gmail.com>
2016-11-06 15:13:24 +0500

    feat(users): check sessions already registered

--- services/cross-social-science/users/blueprint.py
+++ services/cross-social-science/users/blueprint.py
@@ -1,4 +1,6 @@
 from sanic import Blueprint
+
+from sessions import get_session_service
 from .models import make_models
 from .service import _has_user_service, UserService, \
     _set_default_user_service, _register_user_service
@@ -26,6 +28,14 @@ def user_module_registered(state):
         raise RuntimeError(
             "This blueprint expects you to provide database access! "
             "Use: app.blueprint(bp, db=...)")
+
+    try:
+        get_session_service(sessions_db_name)
+    except (TypeError, ValueError):
+        raise RuntimeError(
+            "This blueprint expects `sessions` blueprint! Register it before"
+            "and pass the `sessions_db_name` options"
+        )
 
     initdb, dropdb, manager, model = make_models(db, db_name, loop)  # noqa
     service = UserService(app, db_name, initdb, dropdb, manager, model)
