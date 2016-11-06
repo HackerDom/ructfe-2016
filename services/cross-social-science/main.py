78cf3038633f2c47a0fd4a08527d7bc10cb59959
pahaz <pahaz.blinov@gmail.com>
2016-11-06 20:19:02 +0500

    refactor(main): use new View

--- services/cross-social-science/main.py
+++ services/cross-social-science/main.py
@@ -1,38 +1,38 @@
 import asyncio
 
+from peewee_async import PostgresqlDatabase
 from sanic import Sanic
 from sanic.response import json, text, html
 from sanic.exceptions import ServerError
-
-from models import database
 
 try:
     import uvloop as async_loop
 except ImportError:
     async_loop = asyncio
 
-import settings
 from sessions import session_blueprint as sessions
 from users import user_blueprint as users
 from blog import blog_blueprint as blog
+from views import View
+import settings
 
 
-def make_app():
+def make_app(view=None, database=None):
+    if not view:
+        view = View({"HTML_TEMPLATES_DIR": settings.TEMPLATES_DIR})
+    if not database:
+        database = PostgresqlDatabase(database=settings.DATABASE)
+
     app = Sanic(__name__)
     app.static('/static', settings.STATIC_DIR)
 
     @app.route("/", methods=['GET'])
     async def test_index(request):
-        from templates import render
-        return html(render('index.html', name='variables'))
+        return html(view.render('index', {'name': 'variables'}))
 
     @app.route("/dynamic/<name>/<id:int>")
-    def test_params(request, name, id:int):
+    async def test_params(request, name, id:int):
         return text("yeehaww {} {}".format(name, id))
-
-    @app.route("/exception")
-    def exception(request):
-        raise ServerError("It's dead jim")
 
     @app.route("/await")
     async def test_await(request):
@@ -45,22 +45,26 @@ def make_app():
     # ----------------------------------------------- #
 
     @app.route("/json")
-    def post_json(request):
+    async def post_json(request):
         return json({"received": True, "message": request.json})
 
     @app.route("/form")
-    def post_json(request):
+    async def post_json(request):
         return json({"received": True, "form_data": request.form,
                      "test": request.form.get('test')})
 
-    @app.route("/query_string")
-    def query_string(request):
+    @app.route("/query")
+    async def query_string(request):
         return json({"parsed": True, "args": request.args, "url": request.url,
                      "query_string": request.query_string})
 
     # ----------------------------------------------- #
     # Exceptions
     # ----------------------------------------------- #
+
+    @app.route("/exception")
+    async def exception(request):
+        raise ServerError("It's dead jim")
 
     @app.exception(ServerError)
     async def test(request, exception):
@@ -80,15 +84,23 @@ def make_app():
 # Run Server
 # ----------------------------------------------- #
 
-if __name__ == '__main__':
+
+def main(debug=False):
     # loop
     loop = async_loop.new_event_loop()
     asyncio.set_event_loop(loop)
     # database
-    database
+    database = PostgresqlDatabase(database=settings.DATABASE)
+    #  templates
+    view = View({"HTML_TEMPLATES_DIR": settings.TEMPLATES_DIR})
     # app
-    app = make_app()
+    app = make_app(view, database)
     app.blueprint(sessions, db=database, db_name='sessions', loop=loop)
     app.blueprint(users, db=database, db_name='users', loop=loop,
                   sessions_db_name='sessions')
-    app.run(host="0.0.0.0", port=8000, loop=loop, debug=True)
+    app.blueprint(blog, db=database, db_name='blog', loop=loop)
+    app.run(host="0.0.0.0", port=8000, loop=loop, debug=debug)
+
+
+if __name__ == '__main__':
+    main(debug=True)
--- services/cross-social-science/settings.py
+++ services/cross-social-science/settings.py
@@ -4,3 +4,5 @@ import os
 BASE_DIR = os.path.abspath(os.path.dirname(__name__))
 TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 STATIC_DIR = os.path.join(BASE_DIR, 'static')
+
+DATABASE = 'test'
